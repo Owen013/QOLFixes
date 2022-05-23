8ccea70c9469534556e7c4ef57a05e79f30a4a6c
Owen_013 <owen@crawfordsix.com>
2022-05-23 11:49:15 -0400

    Added Q to exit dialogue

--- Owen013.QOLFixes/QOLFixes.cs
+++ Owen013.QOLFixes/QOLFixes.cs
@@ -1,11 +1,21 @@
 ﻿using OWML.ModHelper;
-using OWML.Common;
-using UnityEngine;
 
 namespace QOLFixes
 {
     public class QOLFixes : ModBehaviour
     {
+        private void Start()
+        {
+            ModHelper.HarmonyHelper.AddPostfix<CharacterDialogueTree>(
+                "Update", typeof(Patches), nameof(Patches.DialogueTreeUpdate));
+        }
+    }
 
+    public static class Patches
+    {
+        public static void DialogueTreeUpdate(CharacterDialogueTree __instance)
+        {
+            if (OWInput.IsNewlyPressed(InputLibrary.cancel, InputMode.All)) __instance.EndConversation();
+        }
     }
 }
