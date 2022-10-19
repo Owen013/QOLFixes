1b2b915e5fb8e16e23d6e899299643e6767a07da
Owen_013 <owen@crawfordsix.com>
2022-10-19 17:21:32 -0400

    v1.3.0: removed unused screaming code, added 'always glow'

---
+++ Owen013.QOLFixes/Patches.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+
+namespace QOLFixes
+{
+    public static class Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.Update))]
+        public static void DialogueTreeUpdate(CharacterDialogueTree __instance)
+        {
+            if (QOLFixes.Instance.fastDialogueExitEnabled && OWInput.IsNewlyPressed(InputLibrary.cancel, InputMode.Dialogue))
+            {
+                __instance.EndConversation();
+                QOLFixes.Instance.ModHelper.Console.WriteLine($"QOLFixes: Exited dialogue for {__instance._characterName}");
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.StartConversation))]
+        public static void DialogueConversationStart(CharacterDialogueTree __instance)
+        {
+            if (QOLFixes.Instance.noFreezeTimeAtEye && LoadManager.s_currentScene == OWScene.EyeOfTheUniverse)
+            {
+                __instance._timeFrozen = false;
+                OWTime.Unpause(OWTime.PauseType.Reading);
+                QOLFixes.Instance.ModHelper.Console.WriteLine($"QOLFixes: Canceled time freeze for {__instance._characterName} dialogue.");
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ProbePromptReceiver), nameof(ProbePromptReceiver.GainFocus))]
+        [HarmonyPatch(typeof(ProbePromptReceiver), nameof(ProbePromptReceiver.LoseFocus))]
+        public static bool ProbePromptEnterExit()
+        {
+            if (QOLFixes.Instance.noAutoScoutEquip)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GhostEffects), nameof(GhostEffects.SetEyeGlow))]
+        public static void Ghost_SetEyeGlow(GhostEffects __instance)
+        {
+            if (QOLFixes.Instance.eyesAlwaysGlow)
+            {
+                __instance._eyeGlow = 1f;
+            }
+        }
+    }
+}
--- Owen013.QOLFixes/QOLFixes.cs
+++ Owen013.QOLFixes/QOLFixes.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using HarmonyLib;
+﻿using HarmonyLib;
 using OWML.ModHelper;
 using OWML.Common;
 
@@ -8,12 +7,10 @@ namespace QOLFixes
     public class QOLFixes : ModBehaviour
     {
         // Config vars
-        public bool fastDialogueExitEnabled, noFreezeTimeAtEye, noAutoScoutEquip, screamEnabled;
+        public bool fastDialogueExitEnabled, noFreezeTimeAtEye, noAutoScoutEquip, eyesAlwaysGlow;
 
         // Mod vars
         public static QOLFixes Instance;
-        public OWAudioSource screamSource;
-        AudioClip screamClip;
 
         public override void Configure(IModConfig config)
         {
@@ -21,86 +18,28 @@ namespace QOLFixes
             fastDialogueExitEnabled = config.GetSettingsValue<bool>("Press Q to Exit Dialogue");
             noFreezeTimeAtEye = config.GetSettingsValue<bool>("Don't Freeze Time at a Certain End-Game Location");
             noAutoScoutEquip = config.GetSettingsValue<bool>("Disable Automatic Scout Equipping");
-            screamEnabled = false;
+            eyesAlwaysGlow = config.GetSettingsValue<bool>("Eyes Always Glow");
         }
 
         public void Awake()
         {
             Instance = this;
+            Harmony.CreateAndPatchAll(typeof(Patches));
         }
 
         public void Start()
         {
-            Harmony.CreateAndPatchAll(typeof(Patches));
-
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                ModHelper.Events.Unity.FireInNUpdates(() =>
-                {
-                    if (loadScene != OWScene.SolarSystem) return;
-                    screamSource = new GameObject(name = "QOL_ScreamSource").AddComponent<OWAudioSource>();
-                    screamSource.transform.parent = FindObjectOfType<PlayerAudioController>().transform;
-                    screamSource.transform.localPosition = Vector3.zero;
-                    screamSource.clip = screamClip;
-                    screamSource.playOnAwake = false;
-                    screamSource.SetMaxVolume(0.03125f);
-                }, 60);
-                    
-
-            };
-            
-        }
-    }
-
-    public static class Patches
-    {
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.Update))]
-        public static void DialogueTreeUpdate(CharacterDialogueTree __instance)
-        {
-            if (QOLFixes.Instance.fastDialogueExitEnabled && OWInput.IsNewlyPressed(InputLibrary.cancel, InputMode.Dialogue))
-            {
-                __instance.EndConversation();
-                QOLFixes.Instance.ModHelper.Console.WriteLine($"QOLFixes: Exited dialogue for {__instance._characterName}");
-            }
+            PrintLog("Quality of Life Changes is ready to go!", MessageType.Success);
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(CharacterDialogueTree), nameof(CharacterDialogueTree.StartConversation))]
-        public static void DialogueConversationStart(CharacterDialogueTree __instance)
+        public void PrintLog(string message)
         {
-            if (QOLFixes.Instance.noFreezeTimeAtEye && LoadManager.s_currentScene == OWScene.EyeOfTheUniverse)
-            {
-                __instance._timeFrozen = false;
-                OWTime.Unpause(OWTime.PauseType.Reading);
-                QOLFixes.Instance.ModHelper.Console.WriteLine($"QOLFixes: Canceled time freeze for {__instance._characterName} dialogue.");
-            }
-        }
-        
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(ProbePromptReceiver), nameof(ProbePromptReceiver.GainFocus))]
-        [HarmonyPatch(typeof(ProbePromptReceiver), nameof(ProbePromptReceiver.LoseFocus))]
-        public static bool ProbePromptEnterExit()
-        {
-            if (QOLFixes.Instance.noAutoScoutEquip)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
+            ModHelper.Console.WriteLine(message);
         }
 
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(GhostBrain), nameof(GhostBrain.ChangeAction), typeof(GhostAction))]
-        public static void GhostChangeAction(GhostBrain __instance)
+        public void PrintLog(string message, MessageType messageType)
         {
-            if (QOLFixes.Instance.screamEnabled && __instance._currentAction.GetName() == GhostAction.Name.Chase)
-            {
-                QOLFixes.Instance.screamSource.Stop();
-                QOLFixes.Instance.screamSource.PlayDelayed(0.5f);
-            }
+            ModHelper.Console.WriteLine(message, messageType);
         }
     }
 }
